4eb479b36a8e0a4774928742419e6dfb3c6a57fe
mfriedl89 <m.friedl89@gmail.com>
2017-05-31 13:06:52 +0200

    [ES, MF] change text fields in edit profile

--- TimemanagementTool/app/src/main/java/layout/EidtProfileFragment.java
+++ TimemanagementTool/app/src/main/java/layout/EidtProfileFragment.java
@@ -2,29 +2,40 @@ package layout;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseUser;
 
 import at.thelegend27.timemanagementtool.Firebase.FirebaseApplication;
+import at.thelegend27.timemanagementtool.HelperClasses.ConfirmFieldValidatorHelper;
+import at.thelegend27.timemanagementtool.HelperClasses.PasswordFieldValidatorHelper;
 import at.thelegend27.timemanagementtool.R;
 import at.thelegend27.timemanagementtool.TimemanagementActivity;
 
-public class EidtProfileFragment extends Fragment {
-    private EditText editProfileName;
-    private EditText editProfileOldPassword;
-    private EditText editProfilePassword;
-    private EditText editProfileRepeatPassword;
-    private ProgressBar progressBar;
+public class EidtProfileFragment extends Fragment implements View.OnFocusChangeListener, View.OnClickListener {
+    private static final int PASS_MIN_LENGTH = 6;
+
+    private TextInputEditText editProfileName;
+    private TextInputEditText editProfileOldPassword;
+    private TextInputEditText editProfilePassword;
+    private TextInputEditText editProfileRepeatPassword;
+    private static ProgressBar progressBar;
+
+    private PasswordFieldValidatorHelper oldPasswordFieldValidator;
+    private PasswordFieldValidatorHelper passwordFieldValidator;
+    private ConfirmFieldValidatorHelper confirmPasswordFieldValidator;
+
+    private final FirebaseApplication firebaseApplication = new FirebaseApplication();
+    private final FirebaseUser user = firebaseApplication.getCurrentFirebaseUser();
 
     @Nullable
     @Override
@@ -39,11 +50,19 @@ public class EidtProfileFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Edit Profile");
 
-        editProfileName = (EditText)getView().findViewById(R.id.profile_name);
-        editProfileOldPassword = (EditText)getView().findViewById(R.id.profile_old_password);
-        editProfilePassword = (EditText)getView().findViewById(R.id.profile_password);
-        editProfileRepeatPassword = (EditText)getView().findViewById(R.id.profile_repeat_password);
+        TextInputLayout oldPasswordWrapper = (TextInputLayout) getView().findViewById(R.id.profile_old_password_wrapper);
+        TextInputLayout passwordWrapper = (TextInputLayout) getView().findViewById(R.id.profile_password_wrapper);
+        TextInputLayout confirmPasswordWrapper = (TextInputLayout) getView().findViewById(R.id.profile_repeat_password_wrapper);
+
+        editProfileName = (TextInputEditText)getView().findViewById(R.id.profile_name);
+        editProfileOldPassword = (TextInputEditText)getView().findViewById(R.id.profile_old_password);
+        editProfilePassword = (TextInputEditText)getView().findViewById(R.id.profile_password);
+        editProfileRepeatPassword = (TextInputEditText)getView().findViewById(R.id.profile_repeat_password);
         progressBar = (ProgressBar) getView().findViewById(R.id.progressBarEditProfil);
+
+        oldPasswordFieldValidator = new PasswordFieldValidatorHelper(oldPasswordWrapper, PASS_MIN_LENGTH);
+        passwordFieldValidator = new PasswordFieldValidatorHelper(passwordWrapper, PASS_MIN_LENGTH);
+        confirmPasswordFieldValidator = new ConfirmFieldValidatorHelper(confirmPasswordWrapper);
 
         Button saveEditButton = (Button)getView().findViewById(R.id.save_edit_button);
         final Button savePasswordButton = (Button)getView().findViewById(R.id.save_password_button);
@@ -55,19 +74,12 @@ public class EidtProfileFragment extends Fragment {
             editProfileName.setText(user.getDisplayName());
         }
 
-        saveEditButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                saveUserProfile(user, firebaseApplication);
-            }
-        });
+        editProfileOldPassword.setOnFocusChangeListener(this);
+        editProfilePassword.setOnFocusChangeListener(this);
+        editProfileRepeatPassword.setOnFocusChangeListener(this);
 
-        savePasswordButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                saveUserPassword(user, firebaseApplication);
-            }
-        });
+        saveEditButton.setOnClickListener(this);
+        savePasswordButton.setOnClickListener(this);
     }
 
     private void saveUserProfile(FirebaseUser user, FirebaseApplication firebaseApplication) {
@@ -80,7 +92,7 @@ public class EidtProfileFragment extends Fragment {
             return;
         }
 
-        progressBar.setVisibility(View.VISIBLE);
+        visibilityProgressbarEditProfile(View.VISIBLE);
 
         if (user != null) {
             String userId = user.getProviderId();
@@ -98,16 +110,7 @@ public class EidtProfileFragment extends Fragment {
 
         TimemanagementActivity.hideKeyboard(getActivity());
 
-        if (TextUtils.isEmpty(profileOldPassword) || TextUtils.isEmpty(profilePassword) || TextUtils.isEmpty(profileRepeatPassword)) {
-            Toast.makeText(getActivity(), "Password required", Toast.LENGTH_LONG).show();
-            return;
-        }
-        if (!TextUtils.equals(profilePassword, profileRepeatPassword)) {
-            Toast.makeText(getActivity(), "Passwords aren't equal. Try again.", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        progressBar.setVisibility(View.VISIBLE);
+        visibilityProgressbarEditProfile(View.VISIBLE);
 
         if (user != null) {
             String userId = user.getProviderId();
@@ -116,6 +119,51 @@ public class EidtProfileFragment extends Fragment {
 
             firebaseApplication.changeUserPassword(getActivity(), user, profileOldPassword, profilePassword, progressBar);
         }
+    }
 
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+        if (hasFocus) {
+            return; //we want to validate only fields loosing focus and not fields gaining focus
+        }
+
+        int id = view.getId();
+        if (id == R.id.profile_old_password) {
+            oldPasswordFieldValidator.validate(editProfileOldPassword.getText().toString());
+        } else if (id == R.id.profile_password) {
+            passwordFieldValidator.validate(editProfilePassword.getText().toString());
+        } else if (id == R.id.profile_repeat_password) {
+            confirmPasswordFieldValidator.confirm(editProfilePassword.getText().toString(), editProfileRepeatPassword.getText().toString());
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.save_edit_button) {
+            saveUserProfile(user, firebaseApplication);
+        }
+        else if (id == R.id.save_password_button) {
+            String enteredOldPassword = editProfileOldPassword.getText().toString();
+            String enteredPassword = editProfilePassword.getText().toString();
+            String enteredConfirmPassword = editProfileRepeatPassword.getText().toString();
+
+            boolean isOldPasswordValid= passwordFieldValidator.validate(enteredOldPassword);
+            boolean isPasswordValid= passwordFieldValidator.validate(enteredPassword);
+            boolean isConfirmPasswordValid= confirmPasswordFieldValidator.confirm(enteredPassword, enteredConfirmPassword);
+
+            if (!isOldPasswordValid || !isPasswordValid || !isConfirmPasswordValid) {
+                //go ahead ans submit the form for all things are fine now
+                Toast.makeText(getActivity(), "Field Validations failed! Please check your inputs", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            saveUserPassword(user, firebaseApplication);
+        }
+    }
+
+    public static void visibilityProgressbarEditProfile(int visibility) {
+        progressBar.setVisibility(visibility);
     }
 }
