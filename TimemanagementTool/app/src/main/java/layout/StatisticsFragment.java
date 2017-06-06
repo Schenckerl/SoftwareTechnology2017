7a271a9f9aeba48ce5f57b1ca00582b833c4b59b
mfriedl89 <m.friedl89@gmail.com>
2017-06-06 12:36:21 +0200

    [MF, MT] statistics for day and week with dummy data

--- TimemanagementTool/app/src/main/java/layout/StatisticsFragment.java
+++ TimemanagementTool/app/src/main/java/layout/StatisticsFragment.java
@@ -1,8 +1,10 @@
 package layout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,23 +18,39 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
+import at.thelegend27.timemanagementtool.HelperClasses.CurrentSession;
 import at.thelegend27.timemanagementtool.R;
+import at.thelegend27.timemanagementtool.database.User;
 
 public class StatisticsFragment extends Fragment implements TabHost.OnTabChangeListener {
     private BarChart barChart;
+    private float[] dayEntriesFloatArray ;
+    private float[][] weekEntriesFloatArray ;
     private ArrayList<BarEntry> dayEntries ;
+    private ArrayList<BarEntry> dayEntriesSetpoint ;
     private ArrayList<BarEntry> weekEntries ;
+    private ArrayList<BarEntry> weekEntriesSetpoint ;
     private ArrayList<BarEntry> monthEntries ;
     private ArrayList<String> barDayEntryLabels ;
+    private ArrayList<String> barDayEntryLabelsSetpoint ;
     private ArrayList<String> barWeekEntryLabels ;
+    private ArrayList<String> barWeekEntryLabelsSetpoint ;
     private ArrayList<String> barMonthEntryLabels ;
     private BarDataSet barDayDataSet ;
+    private BarDataSet barDayDataSetSetpoint ;
     private BarDataSet barWeekDataSet ;
+    private BarDataSet barWeekDataSetSetpoint ;
     private BarDataSet barMonthDataSet ;
     private BarData barDayData ;
     private BarData barWeekData ;
     private BarData barMonthData ;
     private TabHost tabHost;
+    private User currentUser;
+
+    private float groupSpace = 0.06f;
+    private float barSpace = 0.02f; // x2 dataset
+    private float barWidth = 0.25f; // x2 dataset
+
 
     @Nullable
     @Override
@@ -71,56 +89,128 @@ public class StatisticsFragment extends Fragment implements TabHost.OnTabChangeL
         spec.setIndicator("Month");
         tabHost.addTab(spec);
 
+        currentUser = CurrentSession.getInstance().getCurrent_user();
+
+        if (currentUser == null) {
+            Log.d("STATISTICS", "no currentuser available");
+        }
     }
 
     private void setupDayBar() {
         barChart = (BarChart) getActivity().findViewById(R.id.chart_day);
         dayEntries = new ArrayList<>();
-        barDayEntryLabels = new ArrayList<String>();
+        dayEntriesFloatArray = new float[3];
+        dayEntriesSetpoint = new ArrayList<>();
+        barDayEntryLabels = new ArrayList<>();
+
         AddValuesToDayEntries();
         AddValuesToDayBarEntryLabels();
-        barDayDataSet = new BarDataSet(dayEntries, "");
-        barDayData = new BarData(barDayEntryLabels, barDayDataSet);
+        dayEntries.add(new BarEntry(0, dayEntriesFloatArray));
+
+        barDayDataSet = new BarDataSet(dayEntries, "Actual value");
+        barDayDataSetSetpoint = new BarDataSet(dayEntriesSetpoint, "Setpoint");
+
+        barDayData = new BarData(barDayDataSet, barDayDataSetSetpoint);
+
         barDayDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDayDataSetSetpoint.setColor(Color.GRAY);
+
+        barDayData.setBarWidth(barWidth);
+
         barChart.setData(barDayData);
-        barChart.animateY(3000);
+        barChart.groupBars(0, groupSpace, barSpace);
+        barChart.getDescription().setEnabled(false);
+        barChart.getXAxis().setDrawLabels(false);
+        barChart.setFitBars(true);
+        barChart.getXAxis().removeAllLimitLines();
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getXAxis().setAxisMinimum(-0.1f);
+        barChart.getXAxis().setAxisMaximum(0.67f);
+
+        barChart.getAxisLeft().setDrawAxisLine(false);
+        barChart.setTouchEnabled(false);
+
+        barChart.invalidate();
+
+        barChart.animateY(2000);
     }
 
     private void setupWeekBar() {
         barChart = (BarChart) getActivity().findViewById(R.id.chart_week);
         weekEntries = new ArrayList<>();
-        barWeekEntryLabels = new ArrayList<String>();
+        weekEntriesFloatArray = new float[5][3];
+        weekEntriesSetpoint = new ArrayList<>();
+        barWeekEntryLabels = new ArrayList<>();
+
         AddValuesToWeekEntries();
         AddValuesToWeekBarEntryLabels();
-        barWeekDataSet = new BarDataSet(weekEntries, "");
-        barWeekData = new BarData(barWeekEntryLabels, barWeekDataSet);
+
+        barWeekDataSet = new BarDataSet(weekEntries, "Actual value");
+        barWeekDataSetSetpoint = new BarDataSet(weekEntriesSetpoint, "Setpoint");
+
+        barWeekData = new BarData(barWeekDataSet, barWeekDataSetSetpoint);
+        barWeekData.setBarWidth(barWidth);
+
         barWeekDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barWeekDataSetSetpoint.setColor(Color.GRAY);
+
+        barDayData.setBarWidth(barWidth);
+
         barChart.setData(barWeekData);
-        barChart.animateY(3000);
+        barChart.groupBars(0, groupSpace, barSpace);
+        barChart.getDescription().setEnabled(false);
+        barChart.getXAxis().setDrawLabels(false);
+        barChart.setFitBars(true);
+        barChart.getXAxis().removeAllLimitLines();
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getXAxis().setAxisMinimum(0f);
+        barChart.getXAxis().setAxisMaximum(3f);
+
+        barChart.getAxisLeft().setDrawAxisLine(false);
+        barChart.setTouchEnabled(false);
+
+        barChart.invalidate();
+
+        barChart.animateY(2000);
     }
 
-    public void AddValuesToDayEntries(){
-
-        for (int i = 0; i < 3; i++) {
-            dayEntries.add(new BarEntry(3*(1 + i), i));
+    private int getDayTargetHours() {
+        if (currentUser == null) {
+            Log.d("STATISTICS", "Current user is null");
+            return 8;
         }
+        return (currentUser.targetHours / 5);
     }
 
-    public void AddValuesToWeekEntries(){
+    private void AddValuesToDayEntries(){
+
+        for (int i = 0; i < 3; i++) { // TODO: tasks for a day
+            dayEntriesFloatArray[i] = (float) 3*(1 + i);
+        }
+
+        dayEntriesSetpoint.add(new BarEntry(0, getDayTargetHours()));
+    }
+
+    private void AddValuesToWeekEntries(){
 
         for (int i = 0; i < 5; i++) {
-            weekEntries.add(new BarEntry(i, i));
+            for (int j = 0; j < 3; j++) { // TODO: tasks for a day
+                weekEntriesFloatArray[i][j] = (float) i * (1 + j);
+            }
+            weekEntries.add(new BarEntry(i, weekEntriesFloatArray[i]));
+
+            weekEntriesSetpoint.add(new BarEntry(i, getDayTargetHours()));
         }
     }
 
-    public void AddValuesToDayBarEntryLabels(){
+    private void AddValuesToDayBarEntryLabels(){
 
         barDayEntryLabels.add("Task 1");
         barDayEntryLabels.add("Task 2");
         barDayEntryLabels.add("Task 3");
     }
 
-    public void AddValuesToWeekBarEntryLabels(){
+    private void AddValuesToWeekBarEntryLabels(){
 
         barWeekEntryLabels.add("Monday");
         barWeekEntryLabels.add("Tuesday");
@@ -142,3 +232,4 @@ public class StatisticsFragment extends Fragment implements TabHost.OnTabChangeL
         }
     }
 }
+
